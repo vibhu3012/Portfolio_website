1ce85240068d87f9f053adc14692dc85632df7ca
vibhu3012 <71942165+vibhu3012@users.noreply.github.com>
2022-07-07 15:26:42 +0530

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,6 @@
+<?php
+      if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['description'])){    	
+$name = $_POST['name'];
+$email = $_POST['email'];
+$description = $_POST['description'];}
+?>
---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+// if(isset($_POST['submit'])){
+// $name = $_POST['name'];
+// $email= $_POST['email'];
+// $subject= $_POST['subject'];
+// $description= $_POST['description'];
+// }
+echo "SUCCESSful";
+
+?>
